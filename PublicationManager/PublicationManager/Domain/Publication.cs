d033d67b87ee7402277dec7b21fc8ba9d11726bb
Hendrik Lösch <loeschhendrik@gmail.com>
2017-11-21 09:48:18 +0100

    Added domain model
    sadsad

---
+++ PublicationManager/PublicationManager/Domain/Publication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PublicationManager.Domain
+{
+    public class Publication
+    {
+        public string Title { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public string Comment { get; set; }
+
+        public Medium Medium { get; set; }
+    
+    }
+}
