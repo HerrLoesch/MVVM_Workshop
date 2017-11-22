629d71494429e35f0707647918dd6e4f680a58e7
Hendrik Lösch <loeschhendrik@gmail.com>
2017-11-22 10:43:38 +0100

    Added filter beahvior for textboxes

---
+++ PublicationManager/PublicationManager/MVVM/FilterInputBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace PublicationManager.MVVM
+{
+    public class FilterInputBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.PreviewKeyDown += AssociatedObjectOnPreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.PreviewKeyDown -= AssociatedObjectOnPreviewKeyDown;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnPreviewKeyDown(object sender, KeyEventArgs keyEventArgs)
+        {
+            keyEventArgs.Handled = keyEventArgs.Key == Key.A;
+        }
+    }
+}
