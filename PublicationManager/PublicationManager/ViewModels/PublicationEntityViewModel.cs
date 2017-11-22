7b48aae69b186ced4dd196346462279638c9f379
Hendrik Lösch <loeschhendrik@gmail.com>
2017-11-22 11:01:52 +0100

    Added plain IDataErrorInfo

--- PublicationManager/PublicationManager/ViewModels/PublicationEntityViewModel.cs
+++ PublicationManager/PublicationManager/ViewModels/PublicationEntityViewModel.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using GalaSoft.MvvmLight;
 using PublicationManager.Domain;
 using Tynamix.ObjectFiller;
 
 namespace PublicationManager.ViewModels
 {
-    public class PublicationEntityViewModel : ObservableObject
+    public class PublicationEntityViewModel : ObservableObject, IDataErrorInfo
     {
         private string title;
         private DateTime publicationDate;
         private string comment;
         private Medium selectedMedium;
         private IEnumerable<Medium> media;
+        private bool hasErros;
 
         public string Title
         {
@@ -61,5 +63,31 @@ namespace PublicationManager.ViewModels
             PublicationDate = publication.PublicationDate;
             Comment = publication.Comment;
         }
+
+        public string this[string columnName]
+        {
+            get { return ValidateProperty(columnName); }
+        }
+
+        private string ValidateProperty(string propertyName)
+        {
+            if (propertyName == nameof(Title) && string.IsNullOrWhiteSpace(Title))
+            {
+                HasErros = true;
+                return "No title was given.";
+            }
+
+            HasErros = false;
+
+            return string.Empty;
+        }
+
+        public bool HasErros
+        {
+            get { return hasErros; }
+            set { Set(ref hasErros, value); }
+        }
+
+        public string Error { get { return null; } }
     }
 }
